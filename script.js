604f2259ef7d96049f54624384008bf7f8bd0269
olivia <ol.grav1ty33@gmail.com>
2021-07-13 10:26:07 -1000

    Display Points

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
-/* Psuedo-Code
-html: h1 text on top of page: choose one: rock, paper or scissors     X
-html: show buttons of each choice                                     X
-js: when clicked, shows computer and your choice, then shows results  
-js: presents computer and your points
-js: repeats 5 times
-js: at 5th round, show results based on past wins and display winner
-*/
-//ask why this is null
 const container = document.getElementById("container");
 
-const round = document.getElementById("round");
+let countRound = 0;
 
 const playerChoiceButtons = document.getElementById("player-choice-buttons");
 const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
 
-const playerPoints = document.getElementById("player-points");
-const computerPoints = document.getElementById("computer-points");
+let playerPoints = 0/* document.getElementById("player-points") */;
+let computerPoints = 0/* document.getElementById("computer-points") */;
+
+const playerResults = document.getElementById("player-results");
+let computerResults = document.getElementById("computer-results");
 
 let results = document.getElementById("results");
 const playerChoice = document.getElementById("player-choice");
 const computerChoice = document.getElementById("computer-choice");
+
+const winner = document.getElementById("winner");
 
 function computerPlay() {
     let computerChoice = Math.floor(Math.random() * 12) + 1;
@@ -34,55 +30,74 @@ function computerPlay() {
     }
 }
 
+function displayPoints() {
+    playerResults.textContent = `Your Points: ${playerPoints}`;
+    computerResults.textContent = `Computer's Points: ${computerPoints}`;
+}
+
 function round(playerSelection) {
     const computerSelection = computerPlay();
 
-    //Display Results
-    playerChoice.textContent = "You chose: " + playerSelection + ".";       
-    computerChoice.textContent = "The computer chose: " + computerSelection + ".";
-    //results.textContent = `The computer chose: ${computerSelection}.`;
+    playerChoice.textContent =  `Your Chose: ${playerSelection}`;
+    computerChoice.textContent =  `The Computer Chose: ${computerSelection}`;
     //Tie
     if (playerSelection == computerSelection) {
         results.textContent = "It's a Tie!";
     //Player Wins Conditions
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        ++playerPoints;
         results.textContent = "You Won! Your Rock easily smashes the computer's scissor blades.";
+        playerPoints++;
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        ++playerPoints;
         results.textContent = "You Won! Your sharp Scissor blades slice through the computer's paper.";
+        playerPoints++;
     } else if (playerSelection == "paper" && computerSelection == "rock") {
-        ++playerPoints;
         results.textContent = "You Won! Your paper cunningly enfolds the computer's rock.";
+        playerPoints++;
     //Computer Wins Conditions
     } else if (computerSelection == "rock" && playerSelection == "scissors") {
-        ++computerPoints;
         results.textContent = "You Lost! The computer's Rock easily smashes your scissor blades.";
+        computerPoints++;
     } else if (computerSelection == "scissors" && playerSelection == "paper") {
-        ++computerPoints;
         results.textContent = "You Lost! The computer's sharp Scissor blades slice through your paper.";
+        computerPoints++;
     } else {
-        ++computerPoints;
         results.textContent = "You Lost! The computer's paper cunningly enfolds your rock.";
+        computerPoints++;
     }
+    displayPoints();
+    countRound++;
+  /*   if (playerPoints === 5 || computerPoints == 5) {
+        if (playerPoints > computerPoints) {
+            if (playerPoints - computerPoints == 1) {
+                winner.textContent = "You defeated the computer by 1 point and won the RPS Championship!";
+            } else {
+                winner.textContent = `You defeated the computer by ${playerPoints - computerPoints} points and won the RPS Championship!`;
+            }
+        } else if (playerPoints < computerPoints) {
+            if (computerPoints - playerPoints == 1) {
+                winner.textContent = "The computer destroyed you by 1 point and won the RPS Championship! :(";
+            } else {
+                winner.textContent = `The computer destroyed you by ${computerPoints - playerPoints} points and won the RPS Championship! :(`;
+            }
+        }
+    } */
 }
 
 /*
 function game() {
     for(let i = 1; i < 6; i++) {
         if (i == 1) {
-            round.textContent = "----- Round 1 -----";
-            playerPoints.textContent = `Your Points: ${playerPoints}`;
-            computerPoints.textContent = `Computer's Points: ${computerPoints}`;
-            playerChoiceButtons.removeEventListener("click", e=> {
+            playerPoints.textContent = s"Your Points: " + playerPoints;
+            computerPoints.textContent = "Computer's Points: " + computerPoints;
+            playerChoiceButtons.removeEventListener("click", e => {
                 game();
-            })
+            });
         } else if (i == 5) {
-            round.textContent = "----- Final Round -----";
+            countRound.textContent = "Final Round";
             playerPoints.textContent = `Your Points: ${playerPoints}`;
             computerPoints.textContent = `Computer's Points: ${computerPoints}`;
         } else {
-            round.textContent = `----- Round ${i} -----`;
+            countRound.textContent = `Round ${i}`;
             playerPoints.textContent = `Your Points: ${playerPoints}`;
             computerPoints.textContent = `Computer's Points: ${computerPoints}`;
         }
@@ -127,17 +142,14 @@ function game() {
     }
 }
 */
-/*
-playerChoiceButtons.addEventListener("click", e => {
-    game();
-})
-*/
-rock.addEventListener("click", e => {
+
+rock.addEventListener("click", () => {
     round("rock");
 });
-paper.addEventListener("click", e => {
+paper.addEventListener("click", () => {
     round("paper");
 });
-scissors.addEventListener("click", e => {
+scissors.addEventListener("click", () => {
     round("scissors");
 });
+
